16a30f193a4d3d8cdf91326d0c153cc5f4731f14
guineaapphub <guineagamehub@gmail.com>
2026-07-22 10:44:44 +0100

    Fix Leeds club fixtures link

--- app/club-fixtures/FixtureFilters.tsx
+++ app/club-fixtures/FixtureFilters.tsx
@@ -179,15 +179,16 @@ function getCreatorCount(creators: Creator[], club: string) {
 
 function clubSlug(club: string) {
   const slugMap: Record<string, string> = {
-    "Manchester United": "man-united",
-    "Manchester City": "man-city",
-    "Tottenham Hotspur": "tottenham",
-    "Newcastle United": "newcastle",
-    "Nottingham Forest": "nottm-forest",
-    "Wolverhampton Wanderers": "wolves",
-    "West Ham United": "west-ham",
-    "Brighton & Hove Albion": "brighton",
-  };
+  "Manchester United": "man-united",
+  "Manchester City": "man-city",
+  "Tottenham Hotspur": "tottenham",
+  "Newcastle United": "newcastle",
+  "Nottingham Forest": "nottm-forest",
+  "Wolverhampton Wanderers": "wolves",
+  "West Ham United": "west-ham",
+  "Brighton & Hove Albion": "brighton",
+  Leeds: "leeds-united",
+};
 
   if (slugMap[club]) return slugMap[club];
 
